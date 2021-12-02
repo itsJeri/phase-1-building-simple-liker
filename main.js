14a02a0479d37ed418e1d80b1a3c99eef9e7a506
Jerry Tong <zihangtong@gmail.com>
2021-12-01 23:33:06 -0500

    commit tree 7e50151e3bfbba4649876f3408720cdd07a3073d

--- main.js
+++ main.js
@@ -3,9 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const init = () => {
+  const likeButton = document.querySelectorAll('.like-glyph')
+  // target each likeButton individually
+  for(const glyph of likeButton)
+  glyph.addEventListener('click', heartCallback)
+}
+
+  function heartCallback(e) {
+    // define event target on specific likeButton
+    const heart = e.target;
+    mimicServerCall()
+      .then((res) => {
+        // toggles red color
+        heart.classList.toggle('activated-heart')
+        // toggles Full/Empty heart
+        if(heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART
+        } else if (heart.innerText === FULL_HEART) {
+            heart.innerText = EMPTY_HEART;
+          }
+        }
+      )
+      .catch((err) => {
+        // removes .hidden to show error
+        document.querySelector('.hidden').classList.remove('hidden');
+
+        setTimeout(() => {
+          // adds .hiiden to hide error again
+          document.querySelector('#modal').classList.add('hidden');
+        }, 3000); // after 3 seconds
+      })
+  }
+
+  
 
 
-
+document.addEventListener('DOMContentLoaded', init);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
